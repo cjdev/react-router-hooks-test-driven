99ea9193f53fdbf292276238b52b2b465dcfa310
Sean Shubin <sshubin@cj.com>
2020-10-29 14:20:38 -0700

    example of how children are passed as a property

--- src/prototype/learn-class-vs-functional-component.test.js
+++ src/prototype/learn-class-vs-functional-component.test.js
@@ -7,15 +7,28 @@ class MyClassComponent extends React.Component {
     }
 }
 
-const MyFunctionalComponent = ({name}) => <h1>Hello, {name}</h1>
-
 test('render class component', () => {
     const renderResult = render(<MyClassComponent name={'the-name'}/>)
     expect(renderResult.getByText('Hello, the-name')).toBeInTheDocument()
 
 })
 
+const MyFunctionalComponent = ({name}) => <h1>Hello, {name}</h1>
+
 test('render functional component', () => {
     const renderResult = render(<MyFunctionalComponent name={'the-name'}/>)
     expect(renderResult.getByText('Hello, the-name')).toBeInTheDocument()
+})
+
+const FunctionalComponentWithChildren = ({children}) => <h1>hello {children} world</h1>
+
+test('functional component with children', () => {
+    const renderResult = render(<FunctionalComponentWithChildren>
+        <div>child 1</div>
+        <div>child 2</div>
+    </FunctionalComponentWithChildren>)
+    renderResult.debug()
+    expect(renderResult.getByText('hello world')).toBeInTheDocument()
+    expect(renderResult.getByText('child 1')).toBeInTheDocument()
+    expect(renderResult.getByText('child 2')).toBeInTheDocument()
 })
